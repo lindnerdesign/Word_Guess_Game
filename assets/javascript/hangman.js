f596b5520d70e9dd1f603727b2fe505a729ae679
Lindsay Lindner <lindsaylindner@Lindsays-MacBook-Pro.local>
2018-05-28 07:16:38 -0500

    final upload

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,30 +1,26 @@
 //Random word picked from array
 var selectableWords = ["cowboys", "wagonwheel", "revolver", "bonfire", "ranch", "saddle"];
 
-const maxTries = 10;            // Maximum number of tries player has
+const maxTries = 10;
 
-var guessedLetters = [];        // Stores the letters the user guessed
-var currentWordIndex;           // Index of the current word in the array
-var guessingWord = [];          // This will be the word we actually build to match the current word
-var remainingGuesses = 0;       // How many tries the player has left
-var gameStarted = false;        // Flag to tell if the game has started
-var hasFinished = false;        // Flag for 'press any key to try again'
+var guessedLetters = [];
+var currentWordIndex;
+var guessingWord = [];
+var remainingGuesses = 0;
+var gameStarted = false;
+var hasFinished = false;
 var wins = 0;
 
-// Reset our game-level variables
+// Game reset
 function resetGame() {
     remainingGuesses = maxTries;
     gameStarted = false;
 
-    // Use Math.floor to round the random number down to the nearest whole.
     currentWordIndex = Math.floor(Math.random() * (selectableWords.length));
 
-    // Clear out arrays
     guessedLetters = [];
     guessingWord = [];
 
-
-    // Build the guessing word and clear it out
     for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
         guessingWord.push("_");
     }
@@ -37,25 +33,24 @@ function resetGame() {
     updateDisplay();
 };
 
-//  Updates the display on the HTML Page
+// Updates what is showing
 function updateDisplay() {
 
     document.getElementById("totalWins").innerText = wins;
     document.getElementById("currentWord").innerText = "";
+
     for (var i = 0; i < guessingWord.length; i++) {
         document.getElementById("currentWord").innerText += guessingWord[i];
     }
     document.getElementById("remainingGuesses").innerText = remainingGuesses;
     document.getElementById("guessedLetters").innerText = guessedLetters;
+
     if(remainingGuesses <= 0) {
         document.getElementById("gameover-image").style.cssText = "display: block";
         document.getElementById("pressKeyTryAgain").style.cssText = "display:block";
         hasFinished = true;
     }
 };
-
-// Updates the image depending on how many guesses
-
 
 document.onkeydown = function(event) {
     // If we finished a game, dump one keystroke and reset.
@@ -76,31 +71,25 @@ function makeGuess(letter) {
             gameStarted = true;
         }
 
-        // Make sure we didn't use this letter yet
         if (guessedLetters.indexOf(letter) === -1) {
             guessedLetters.push(letter);
             evaluateGuess(letter);
         }
     }
-
     updateDisplay();
     checkWin();
 };
 
-// This function takes a letter and finds all instances of
-// appearance in the string and replaces them in the guess word.
 function evaluateGuess(letter) {
-    // Array to store positions of letters in string
+
     var positions = [];
 
-    // Loop through word finding all instances of guessed letter, store the indicies in an array.
     for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
         if(selectableWords[currentWordIndex][i] === letter) {
             positions.push(i);
         }
     }
 
-    // if there are no indicies, remove a guess and update the hangman image
     if (positions.length <= 0) {
         remainingGuesses--;
 
